b576b9a51423395ec7500e44ef272d7dc2844c5b
Kazuhito Hokamura <k.hokamura@gmail.com>
2018-04-03 21:52:42 +0900

    Refactor User

--- lib/domains/user.dart
+++ lib/domains/user.dart
@@ -9,28 +9,46 @@ class User {
   final int id;
   final String name;
 
+  static final String endpoint = 'http://localhost:3000';
+
+  static _get(String path, { auth: true }) async {
+    var url = '$endpoint$path';
+    var headers = {};
+    if (auth) {
+      var token = await getSavedToken();
+      headers['Authorization'] = 'Bearea $token';
+    }
+    var response = await http.get(url, headers: headers);
+    return json.decode(response.body);
+  }
+
+  static _post(String path, body, { auth: true }) async {
+    var url = '$endpoint$path';
+    var headers = { 'content-type': 'application/json; charset=utf-8' };
+    if (auth) {
+      var token = await getSavedToken();
+      headers['Authorization'] = 'Bearea $token';
+    }
+    var response = await http.post(url, body: json.encode(body), headers: headers);
+    return json.decode(response.body);
+  }
+
   static Future<String> getSavedToken() async {
     var prefs = await SharedPreferences.getInstance();
     return prefs.getString('token');
   }
 
   static Future<bool> login(String name, String password) async {
-    var url = 'http://localhost:3000/user_token';
     var body = { "auth": { "name": name, "password": password } };
-    var headers = { 'content-type': 'application/json; charset=utf-8' };
-    var response = await http.post(url, body: json.encode(body), headers: headers);
-    var token = json.decode(response.body)['jwt'];
+    var response = await _post('/user_token', body);
+    var token = response['jwt'];
     var prefs = await SharedPreferences.getInstance();
     prefs.setString('token', token);
     return true;
   }
 
   static Future<User> getCurrentUser() async {
-    var token = await getSavedToken();
-    var url = 'http://localhost:3000/current_user';
-    var headers = { 'Authorization': 'Bearea $token' };
-    var response = await http.get(url, headers: headers);
-    var user = json.decode(response.body);
+    var user = await _get('/current_user');
     return new User(id: user['id'], name: user['name']);
   }
 }
