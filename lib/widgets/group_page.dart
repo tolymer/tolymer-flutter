5b1a050217d2a68cc0c9384e0ed55a82a9b364e2
Kazuhito Hokamura <k.hokamura@gmail.com>
2018-04-05 22:49:59 +0900

    List event members

--- lib/widgets/group_page.dart
+++ lib/widgets/group_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import '../domains/group.dart';
 import 'new_event_page.dart';
+import '../domains/group.dart';
 import '../domains/event.dart';
+import '../domains/user.dart';
 
 class GroupPage extends StatefulWidget {
   GroupPage({ Key key, this.group }) : super(key: key);
@@ -12,24 +13,59 @@ class GroupPage extends StatefulWidget {
   _GroupPageState createState() => new _GroupPageState();
 }
 
-class _GroupPageState extends State<GroupPage> {
+class _GroupPageState extends State<GroupPage> with SingleTickerProviderStateMixin {
   List<Event> _events = [];
+  List<User> _members = [];
+  TabController _controller;
 
   _setup() async {
     var events = await widget.group.getEvents();
-    setState(() => _events = events);
+    var members = await widget.group.getMembers();
+    setState(() {
+      _events = events;
+      _members = members;
+    });
   }
 
   @override
   void initState() {
     super.initState();
+    _controller = new TabController(vsync: this, length: 2);
     _setup();
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.group.name),
+        bottom: new TabBar(
+          controller: _controller,
+          tabs: [
+            new Tab(text: 'Events'),
+            new Tab(text: 'Members'),
+          ]
+        ),
+      ),
+      body: new TabBarView(
+        controller: _controller,
+        children: [_eventListView(), _membersListView()],
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _openNewEventPage,
+        tooltip: 'New Event',
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+
+  _eventListView() {
     Iterable<Widget> listTiles = _events.map((event) {
       return new ListTile(
         leading: new Icon(Icons.explicit),
@@ -42,16 +78,21 @@ class _GroupPageState extends State<GroupPage> {
       );
     });
     listTiles = ListTile.divideTiles(context: context, tiles: listTiles);
-    return new Scaffold(
-      appBar: new AppBar(title: new Text(widget.group.name)),
-      body: new ListView(children: listTiles.toList()),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _openNewEventPage,
-        tooltip: 'New Event',
-        child: new Icon(Icons.add),
-      ),
-    );
-}
+
+    return new ListView(children: listTiles.toList());
+  }
+
+  _membersListView() {
+    Iterable<Widget> listTiles = _members.map((user) {
+      return new ListTile(
+        leading: new Icon(Icons.person),
+        title: new Text(user.name),
+      );
+    });
+    listTiles = ListTile.divideTiles(context: context, tiles: listTiles);
+
+    return new ListView(children: listTiles.toList());
+  }
 
   _openNewEventPage() {
     Navigator.push(context, new MaterialPageRoute<DismissDialogAction>(
